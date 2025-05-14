0e2e3c3ab43a5eb46167d717433973788ae7622f
rashu-pro <rashu.pro@gmail.com>
2025-05-14 17:17:13 +0600

    loader removed

--- assets/home/js/page.js
+++ assets/home/js/page.js
@@ -3,12 +3,12 @@
 jQuery(window).on('load', function () {
     "use strict";
     // will first fade out the loading animation
-    jQuery("#status").fadeOut();
+    // jQuery("#status").fadeOut();
     // will fade out the whole DIV that covers the website.
-    jQuery("#preloader").delay(1000).fadeOut("slow");
-    $("#preloader").fadeOut(1000, function () {
-        $('body').removeClass('loading');
-    });
+    // jQuery("#preloader").delay(1000).fadeOut("slow");
+    // $("#preloader").fadeOut(1000, function () {
+    //     $('body').removeClass('loading');
+    // });
 
 })
 
